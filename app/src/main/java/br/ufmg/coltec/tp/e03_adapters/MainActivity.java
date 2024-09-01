7af9cb081dadf580e7793ac641054bf8685845f2
Leandro <leandroxavyer2@gmail.com>
2024-09-01 10:18:41 -0300

    Add fragment_place_details.xml

--- app/src/main/java/br/ufmg/coltec/tp/e03_adapters/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_adapters/MainActivity.java
@@ -1,8 +1,8 @@
 package br.ufmg.coltec.tp.e03_adapters;
 
-import android.app.Activity;
+
 import android.os.Bundle;
-import android.widget.ListView;
+
 
 import androidx.fragment.app.FragmentActivity;
 
